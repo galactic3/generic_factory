a73b7cba35dbd52178a107b679af17f0e4051124
Alex Aunter <lxaunter@gmail.com>
2022-07-16 01:21:22 +0400

    feat: add authorization and prevent code reset

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::convert::TryInto;
+use std::str;
 
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::{near_bindgen, Promise, env, Gas};
@@ -14,12 +15,38 @@ pub struct FactoryContract {
 #[no_mangle]
 pub extern "C" fn set_code() {
     env::setup_panic_hook();
+
     unsafe {
+        near_sys::current_account_id(0);
+        let mut current_account_id: Vec<u8> = vec![];
+        current_account_id.resize(near_sys::register_len(0) as _, 0);
+        near_sys::read_register(0, current_account_id.as_ptr() as _);
+        let current_account_id: &str = str::from_utf8(&current_account_id).unwrap();
+
+        near_sys::predecessor_account_id(0);
+        let mut predecessor_account_id: Vec<u8> = vec![];
+        predecessor_account_id.resize(near_sys::register_len(0) as _, 0);
+        near_sys::read_register(0, predecessor_account_id.as_ptr() as _);
+        let predecessor_account_id: &str = str::from_utf8(&predecessor_account_id).unwrap();
+
+        assert_eq!(
+            current_account_id,
+            predecessor_account_id,
+            "expected current_account_id as caller",
+        );
+
         // save input to internal register
         near_sys::input(0);
         // set key
         let key = "code".as_bytes();
         near_sys::write_register(1, key.len() as u64, key.as_ptr() as u64);
+
+        assert_eq!(
+            near_sys::storage_has_key(u64::MAX as _, 1 as _),
+            0,
+            "set_code has already been called"
+        );
+
         // save code to the state
         near_sys::storage_write(u64::MAX as _, 1 as _, u64::MAX as _, 0 as _, 2);
         // return true
@@ -33,7 +60,7 @@ impl FactoryContract {
     #[payable]
     pub fn create(&mut self, name: String, init_function: String, init_args: String) -> Promise {
         let account_id: ValidAccountId = format!("{}.{}", name, env::current_account_id()).try_into().unwrap();
-        let code = env::storage_read(&"code".as_bytes()).unwrap();
+        let code = env::storage_read(&"code".as_bytes()).expect("code not set, call set_code first");
         Promise::new(account_id.into())
             .create_account()
             .transfer(env::attached_deposit())
--- tests/main.rs
+++ tests/main.rs
@@ -14,28 +14,63 @@ const NO_DEPOSIT: Balance = 0;
 #[test]
 fn test_deploy_set_code_create_call() {
     let root = init_simulator(None);
+    // deploy works
     let factory = root.deploy(&FACTORY_BYTES, "factory".parse().unwrap(), to_yocto("10"));
     let user = root.create_user("user".into(), to_yocto("10"));
 
-    let res = root.call(factory.account_id(), "set_code", &HELLO_BYTES, SET_CODE_GAS, NO_DEPOSIT);
-    assert!(res.is_ok());
-
-    let init_args: String = json!({ "subject": "world" }).to_string();
+    // create should fail before set_code
     let res = user.call(
         factory.account_id(),
         "create",
         &json!({
             "name": "subaccount",
             "init_function": "new",
-            "init_args": init_args
+            "init_args": json!({ "subject": "world" }).to_string(),
+        }).to_string().into_bytes(),
+        CREATE_GAS,
+        to_yocto("5"),
+    );
+    assert!(!res.is_ok());
+    assert!(format!("{:?}", res.status()).contains("code not set, call set_code first"));
+
+    // set_code by not current_account_id fails
+    let res = user.call(factory.account_id(), "set_code", &HELLO_BYTES, SET_CODE_GAS, NO_DEPOSIT);
+    assert!(!res.is_ok());
+
+    // set_code by current_account_id succeeds
+    let res = factory.call(factory.account_id(), "set_code", &HELLO_BYTES, SET_CODE_GAS, NO_DEPOSIT);
+    assert!(res.is_ok());
+
+    // set_code by current_account_id fails second time
+    let res = factory.call(factory.account_id(), "set_code", &HELLO_BYTES, SET_CODE_GAS, NO_DEPOSIT);
+    assert!(!res.is_ok());
+
+    // create by random user works
+    let res = user.call(
+        factory.account_id(),
+        "create",
+        &json!({
+            "name": "subaccount",
+            "init_function": "new",
+            "init_args": json!({ "subject": "world" }).to_string(),
         }).to_string().into_bytes(),
         CREATE_GAS,
         to_yocto("5"),
     );
     assert!(res.is_ok());
 
+    // init method was called with the correct args
     let res = root.view("subaccount.factory".into(), "hello", &vec![]);
     assert!(res.is_ok());
     let res: String = res.unwrap_json();
     assert_eq!(res, "Hello, world!");
 }
+
+// - [DONE] check that create fails before set_code
+// - [DONE] check that owner can set_code
+// - [DONE] check that non owner cannot set_code
+// - [DONE] check that create works after set_code
+// - [DONE] check that anyone can create
+// - [DONE] check that create calls init with correct attributes
+// - [DONE] check that set_code cannot be called again
+// - [TODO] check that get_code_hash works
